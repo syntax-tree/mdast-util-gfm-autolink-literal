dc059a554f25f2046d6ba4d5c3bf125e97f3e731
Titus Wormer <tituswormer@gmail.com>
2021-07-28 13:39:14 +0200

    Update `xo`

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import test from 'tape'
 import {toHtml} from 'hast-util-to-html'
 import {toHast} from 'mdast-util-to-hast'
